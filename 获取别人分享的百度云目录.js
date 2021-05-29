fef4bca9cfdc3dd0037b244568ed366524ad9acd
Json_var <412032320@qq.com>
2021-05-29 10:53:15 +0800

    Update 获取别人分享的百度云目录.js

--- 获取别人分享的百度云目录.js
+++ 获取别人分享的百度云目录.js
@@ -1,14 +1,30 @@
 $(function(){
+    
+    let baiduPath = "https://pan.baidu.com/share/list?uk=3253638235&shareid=2931971488&order=other&desc=1&showempty=0&web=1&page=1&num=100&dir=%2Fsharelink3253638235-122627231427974%2F3、补充资料-历年试题&t=0.14375705411442619&channel=chunlei&web=1&app_id=250528&bdstoken=731f1cf2f5e4f5338032ccc9f83fad85&logid=NjBFRDEzMDIzRTUxOUVFMzg5NjdEMDcyMjFCMzNCQkQ6Rkc9MQ==&clienttype=0";
     // 此操作需在控制台进行，本地或非百度云盘域名执行会报跨域错误
     // 主目录名称存为数组
     var path = [];
     var totalStr = "";
+    function getQueryVariable(url, variable)
+    {
+        var num = url.indexOf("?");
+       var query = url.substring(num + 1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+    }
+    var beganUrl = "";
+    var endedUrl = "";
+    var middleUrl = "";
     function dir(url,fuhao){
         fuhao += "——";
         $.ajax({
             // 百度网盘接口
             // 路径请点击分享链接的networking获取，只需更改dir即可
-            url:"https://pan.baidu.com/share/list?uk=1627575672&shareid=1722169857&order=other&desc=1&showempty=0&web=1&page=1&num=100&dir= " + url + " &t=0.7747160624242104&channel=chunlei&web=1&app_id=250528&bdstoken=731f1cf2f5e4f5338032ccc9f83fad85&logid=NjBFRDEzMDIzRTUxOUVFMzg5NjdEMDcyMjFCMzNCQkQ6Rkc9MQ==&clienttype=0",
+            url: beganUrl + url + endedUrl,
             dataType:"json",
             async:false,
             success:function(data){
@@ -64,7 +80,13 @@ $(function(){
       save_link.download = name;
       fakeClick(save_link);
     }
-    dir("/sharelink1627575672-167438281392281%2F2020课件","");
+    var realPath = decodeURIComponent(baiduPath);
+    middleUrl = getQueryVariable(realPath, "dir");
+    //  切割字符串
+    var tempArr = realPath.split(middleUrl);
+    beganUrl = tempArr[0];
+    endedUrl = tempArr[1];
+    dir(middleUrl,"");
     console.log(totalStr)
     // console.log(path)
     exportRaw('exportDirList.txt',totalStr)       
